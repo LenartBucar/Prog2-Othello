5accb7d2f9d3684b394eee29bc3bf428e7916d59
LenartBucar <lenart.bucar@student.fmf.uni-lj.si>
2022-04-21 21:17:32 +0200

    Add simple text-based interface to play the game.

--- src/othello/Othello.java
+++ src/othello/Othello.java
@@ -3,11 +3,34 @@ package othello;
 import logika.*;
 import splosno.Poteza;
 
+import java.util.Scanner;
+
 public class Othello {
     public static void main(String[] args) {
-        Igra i1 = new Igra();
-        System.out.println(i1);
-        i1.odigraj(new Poteza(3, 2));
-        System.out.println(i1);
+        play();
+    }
+
+    public static void play() {
+        Igra game = new Igra();
+        Scanner in = new Scanner(System.in);
+        System.out.println(game);
+        boolean cont = true;
+        while (cont) {
+            boolean hasPlayed = false;
+            while (!hasPlayed) {
+                System.out.println("PLAYER " + game.getPlayer());
+                System.out.println("Enter x coordinate:");
+                int x = in.nextInt();
+                if (x == -1) {cont = false; break;}
+                System.out.println("Enter y coordinate:");
+                int y = in.nextInt();
+                hasPlayed = game.odigraj(new Poteza(x, y));
+                if (!hasPlayed) {
+                    System.out.println("Invalid move. Try again.");
+                }
+            }
+            System.out.println(game);
+        }
+
     }
 }
